961fe1b935445f1bf222687f91d8e9ae0eeaaffd
Steve Alvarez <L21211909@tectijuana.edu.mx>
2025-03-05 17:24:55 -0800

    Correccion de la  recuperacion de contraseña

--- components/recuperacion-form .tsx
+++ components/recuperacion-form .tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
