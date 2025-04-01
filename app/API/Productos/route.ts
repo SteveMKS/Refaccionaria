fd4f99dbf2a0bd0a905bef1b92c7998351dd28a4
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-03-31 17:23:15 -0700

    RR

--- app/API/Productos/route.ts
+++ app/API/Productos/route.ts
@@ -8,9 +8,16 @@ export async function GET() {
     if (error) throw error;
 
     return NextResponse.json(data, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 400 });
+} catch (error) {
+    let errorMessage = "Ocurrió un error desconocido";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    return NextResponse.json({ error: errorMessage }, { status: 400 });
   }
+
 }
 
 export async function POST(req: Request) {
