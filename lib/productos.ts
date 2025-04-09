255d17627d63948d7934a298a795dc7137e98a92
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-09 15:20:26 -0700

    Modificacion

--- lib/productos.ts
+++ lib/productos.ts
@@ -1,4 +1,4 @@
-/*import { supabase } from '@/lib/supabase';
+import { supabase } from '@/lib/supabase';
 import { useState, useEffect } from 'react'
 
 // Tipos para las relaciones
@@ -83,4 +83,4 @@ export async function getProductos(): Promise<ProductoFrontend[]> {
   });
 
   return data.map(mapProducto);
-}*/
+}
