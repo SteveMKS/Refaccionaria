30a5251562b06f3732d9e95ebb96430256d69c09
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-13 20:15:01 -0700

    Productos ON

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,7 +6,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://tplkyyoiwcvncjeampep.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbGt5eW9pd2N2bmNqZWFtcGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NTY2MDcsImV4cCI6MjA1NTUzMjYwN30.gEYXGQ7AOq-wpgL31ixFpxU6xlzzRJikjcDpuj8mL2c";
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
       persistSession: false,
       autoRefreshToken: false
