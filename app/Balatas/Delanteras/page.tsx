2088ba16ffbf7562a465bd49210a98a16371a1ca
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-14 20:51:33 -0700

    Zustand Prueba

--- app/Balatas/Delanteras/page.tsx
+++ app/Balatas/Delanteras/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
+"use client";
+import { create } from 'zustand';
+
+const useTestStore = create((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+}));
+
+export default function TestPage() {
+  const { count, increment } = useTestStore();
   return (
-    <h1>Hola Mundo</h1>
+    <div>
+      <p>Contador: {count}</p>
+      <button onClick={increment}>+1</button>
+    </div>
   );
 }
