c7b608b1a8b1bfa1976d312e60a486757ca916f2
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-16 14:03:26 -0700

    ADD Politicas y Modificacion de Carrito.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,12 @@
-// lib/supabase.js
-
 import { createClient } from "@supabase/supabase-js";
 
-// Usa tus credenciales de Supabase
 const supabaseUrl = "https://tplkyyoiwcvncjeampep.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbGt5eW9pd2N2bmNqZWFtcGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NTY2MDcsImV4cCI6MjA1NTUzMjYwN30.gEYXGQ7AOq-wpgL31ixFpxU6xlzzRJikjcDpuj8mL2c";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // Recortado por seguridad
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  });
+  auth: {
+    persistSession: true,
+    storage: localStorage,
+    autoRefreshToken: true,
+  },
+});
