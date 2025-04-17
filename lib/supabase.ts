fdd3d0b118d8fb6c55a0e487120761b67dae7667
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-17 14:06:50 -0700

    Modifcacion de tiempo de Sesion

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+'use client';
 
-const supabaseUrl = "https://tplkyyoiwcvncjeampep.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbGt5eW9pd2N2bmNqZWFtcGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NTY2MDcsImV4cCI6MjA1NTUzMjYwN30.gEYXGQ7AOq-wpgL31ixFpxU6xlzzRJikjcDpuj8mL2c";
+import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-});
+export const supabase = createBrowserClient(
+  'https://tplkyyoiwcvncjeampep.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbGt5eW9pd2N2bmNqZWFtcGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NTY2MDcsImV4cCI6MjA1NTUzMjYwN30.gEYXGQ7AOq-wpgL31ixFpxU6xlzzRJikjcDpuj8mL2c'
+);
