70ecb5309084268910f3c530b3ee30a6600f8d4d
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-16 21:24:32 -0700

    AMVS

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,9 +4,4 @@ const supabaseUrl = "https://tplkyyoiwcvncjeampep.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // Recortado por seguridad
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true,
-    storage: localStorage,
-    autoRefreshToken: true,
-  },
 });
