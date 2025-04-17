05877f8d41db2fc0328d643b13321c14ea5f9a05
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-16 21:47:49 -0700

    MVTS

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://tplkyyoiwcvncjeampep.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // Recortado por seguridad
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbGt5eW9pd2N2bmNqZWFtcGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NTY2MDcsImV4cCI6MjA1NTUzMjYwN30.gEYXGQ7AOq-wpgL31ixFpxU6xlzzRJikjcDpuj8mL2c";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 });
