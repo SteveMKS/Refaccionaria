0448d3bd295fbedfddaaafde98ff8680ba4c8943
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-17 18:27:25 -0700

    SS

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 
 export const createClient = () =>
-  createBrowserClient(
+  createPagesBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
