838395ada069dd6c63baa9724c1228dc2f6b5b3d
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-21 08:40:25 -0700

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,7 +1,6 @@
 import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/types/supabase'; // si tienes types
 
-export const supabase = createBrowserClient<Database>(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
