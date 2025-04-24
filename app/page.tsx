1918d31a340c0c6b3f0221b84622b1170b155567
Steve Alvarez <150864410+SteveMKS@users.noreply.github.com>
2025-04-24 04:17:24 -0700

    ASD

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 to-gray-700 text-white p-6 sm:p-12">
